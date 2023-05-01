6114a5b4924307c138ce973b2e9c6d3a4ccd68e6
Emmanuel <egb6005@rit.edu>
2023-05-01 12:07:09 -0400

    EGB

---
+++ acitivities.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class acitivities {
+    public static product() {
+        while (true) {
+            Scanner userInput = new Scanner(System.in);
+            System.out.println("Enter a number(one at a time): ");
+            if (userInput == "");
+            break;
+            return userInput;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(product());
+    }
+}
